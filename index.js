0eaab52e7b08baa9239e89f82bae9912d585520a
nurlan <baitassovnurlan1507@gmail.com>
2024-10-13 00:34:36 +0500

    init: project structure

---
+++ index.js
@@ -0,0 +1,18 @@
+import readline from 'readline'
+import os from 'os'
+
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+const currentDirectory = os.homedir();
+const printCurrentDirectory = () => {
+    console.log(`You are currently in ${currentDirectory}`);
+};
+
+printCurrentDirectory();
+
+rl.on('line', (input) => {
+    console.log(input)
+}).on('close', () => {
+});
