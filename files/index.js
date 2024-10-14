2446e565b86e8ab89a9da391c8cf759c1378c469
nurlan <baitassovnurlan1507@gmail.com>
2024-10-14 22:43:08 +0500

    feat: rename, add, move and remove operations. TODO: to use function context to pass current location

--- files/index.js
+++ files/index.js
@@ -3,9 +3,12 @@ import path from "path";
 import fs from "fs";
 import fsAsync from 'fs/promises'
 import {isAboveHomeDirectory} from "./helpers.js";
+import pipeline from 'stream'
+import {promisify} from 'util'
 
-const handleFileCommands = async(input,currentPath) => {
-    const [option, command] = input.split(" ");
+
+async function handleFileCommands(option,currentPath){
+
     try {
         switch (option) {
             case 'up':
@@ -37,20 +40,47 @@ const handleFileCommands = async(input,currentPath) => {
                 })
                 break;
             case 'add':
+                //command это имя файла
+                const createStream = fs.createWriteStream(path.join(currentPath, command));
+                createStream.end(() => {
+                    console.log('File ' + command.toString() + ' successfuly created!')
+                })
                 break;
             case 'rn':
+                if (args.length === 0) {
+                    console.log('Invalid input: missing new filename.');
+                } else {
+                    const oldFilePath = path.join(currentPath, command);
+                    const newFilePath = path.join(currentPath, args[0]);
+                    await fsAsync.rename(oldFilePath, newFilePath);
+                    console.log(`File renamed to ${args[0]}`);
+                }
                 break;
             case 'mv':
+                const sourceFilePath = path.join(currentPath, command);
+                const asyncPipeline = promisify(pipeline)
+                const destinationPath = path.resolve(currentPath, args[0], path.basename(command));
+
+                asyncPipeline(
+                    fs.createReadStream(sourceFilePath),
+                    //create copy with new name
+                    fs.createWriteStream(destinationPath)
+                )
+                await fsAsync.unlink(sourceFilePath)
                 break;
-            case 'rm':
+            case 'rm': // Remove file
+                const deleteFilePath = path.join(currentPath, command);
+                await fsAsync.unlink(deleteFilePath);
+                console.log(`File ${command} removed.`);
                 break;
             default:
                 throw new Error(`Invalid command: ${option} not found`)
         }
-    } catch (e) {
-        console.error(e.message)
+    }catch (e) {
+
     }
-};
+}
+
 
 
 export default handleFileCommands
